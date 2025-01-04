aace3beb8f50d566e210ce0746222674d52a7168
KarmaFaber <maria.zolotarova@gmail.com>
2025-01-04 14:51:45 +0100

    notas main

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: mzolotar <mzolotar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 11:33:40 by mzolotar          #+#    #+#             */
-/*   Updated: 2025/01/04 14:45:41 by mzolotar         ###   ########.fr       */
+/*   Updated: 2025/01/04 14:51:25 by mzolotar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,3 +46,37 @@ int	main(int argc, char *argv[], char *envp[])
 	}
 	return (0);
 }
+
+/*
+	if (pid == -1) // Error en fork
+        return (perror_and_return_1(3));
+ // Código para el hijo (PID == 0)
+    else if (pid == 0)
+    {
+        // Redirigir entradas y salidas
+        // Primero, cierras el descriptor de lectura en el hijo porque solo necesita escribir en el pipe
+        close(pipfd[0]); // Cerrar extremo de lectura del pipe en el hijo
+        // Redirige stdout al pipe
+        dup2(pipfd[1], STDOUT_FILENO); // Redirigir stdout al pipe
+        close(pipfd[1]); // Cerrar el pipe original después de redirigir
+
+        // Aquí pones la lógica del hijo: comando1 < archivo1
+        ft_printf("Aquí va la función con procesos hijo\n");
+        // Podrías ejecutar un comando con execvp (por ejemplo: "ls -l") aquí
+        // execvp(argv[1], argv + 1); // Ejemplo de ejecución de un comando
+    }
+    // Código para el padre (PID > 0)
+    else
+    {
+        // Redirigir la entrada estándar para leer del pipe
+        close(pipfd[1]); // Cerrar extremo de escritura del pipe en el padre
+        // Redirigir stdin al pipe
+        dup2(pipfd[0], STDIN_FILENO); // Redirigir stdin al pipe
+        close(pipfd[0]); // Cerrar el pipe original después de redirigir
+
+        // Aquí pones la lógica del padre: comando2 > archivo2
+        ft_printf("Aquí va la función con procesos padre\n");
+        // Podrías ejecutar otro comando con execvp aquí
+        // execvp(argv[3], argv + 3); // Ejemplo de ejecución de un comando
+    }
+*/
